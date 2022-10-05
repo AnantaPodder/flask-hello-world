a4858a2bebbac2114919da6a702e8eba64a4b815
Ananta Podder <31305962+AnantaPodder@users.noreply.github.com>
2022-10-05 15:42:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route('/')
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+import time
+while True:
+    print("x")
+    time.sleep(5)
